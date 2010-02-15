baa830784d697145f07a100ce5f14c30a95b14ee
arthur.ii.yuan <arthur.ii.yuan@fea37274-fc4e-11de-be56-c15834921611>
2010-02-15 14:56:09 +0000

    added testngppst

---
+++ trunk/tests/3rdparty/testngppst/include/testngppst/win32/Die.h
@@ -0,0 +1,14 @@
+
+#ifndef __TESTNGPPST_WIN32_DIE_H
+#define __TESTNGPPST_WIN32_DIE_H
+
+#include <testngppst/testngppst.h>
+
+TESTNGPPST_NS_START
+
+struct Die{};
+
+TESTNGPPST_NS_END
+
+#endif
+
---
+++ trunk/tests/3rdparty/testngppst/src/runner/TestCaseSandboxRunnerDieHandler.cpp
@@ -0,0 +1,13 @@
+
+#include <testngppst/win32/Die.h>
+#include <testngppst/runner/TestCaseRunnerDieHandler.h>
+
+TESTNGPPST_NS_START
+
+void TestCaseRunnerDieHandler::
+die(TestCase* testcase, TestCaseResultCollector* collector)
+{
+   throw Die();
+}
+
+TESTNGPPST_NS_END
