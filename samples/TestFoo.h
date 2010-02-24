341ad661653ee1e432de05c1b34f5fbdb3c7515c
arthur.ii.yuan <arthur.ii.yuan@fea37274-fc4e-11de-be56-c15834921611>
2010-02-24 16:46:32 +0000

    Made windows sample different encodings.

--- samples/TestFoo.h
+++ samples/TestFoo.h
@@ -1,160 +1,42 @@
 
-#if defined(_MSC_VER)
-#include <windows.h>
-#endif
-
-#include <cstdlib>
-#include <string>
-#include <iostream>
-
 #include <testngpp/testngpp.hpp>
 
-USING_TESTNGPP_NS
-
-struct TestFixture12 {};
-
-FIXTURE(Foo, ç±»CFooçš„æµ‹è¯•è£…ç½®, ä½ å¯ä»¥ä½¿ç”¨å®ƒä»¬: ())
+FIXTURE(CFoo, ÖÐÎÄÓÃÀýÑùÀý())
 {
-public:
+	TEST(ÖÐÎÄ())
+	{
+		INFO("yes");
+	}
 
-    // @test
-    TEST(case0)
-    {
-       INFO("cleanup test");
+	TEST(²âÊÔ: 1+1 = 2)
+	{
+		ASSERT_EQ(2, 1+1);
+	}
 
-       __DO__
+	TEST(__DO__/__CLEANUP__/__DONE__)
+	{
+		__DO__
+			FAIL("I'm gonna fail");
+		    WARN("I should not be shown");
+		__CLEANUP__
+			INFO("I should be shown always");
+		__DONE__
+	}
 
-       WARN("gonna have an assertion failure");
+	TEST(again: __DO__/__CLEANUP__/__DONE__)
+	{
+		__DO__
+			INFO("I'm ok");
+		__CLEANUP__
+			INFO("I should be shown always");
+		__DONE__
+	}
 
-       ASSERT_FALSE(true);
-
-       __CLEANUP__
-
-       ASSERT_TRUE(true);
-
-       __DONE__
-    }
-			
-    // @test
-    // @timeout=15s
-    void case1()
-    {
-       __DO__
-
-       ASSERT_TRUE(true);
-
-       __CLEANUP__
-
-       ASSERT_FALSE(true);
-
-       __DONE__
-    }
-
-#if !defined(TESTNGPP_DISABLE_SANDBOX) || !TESTNGPP_DISABLE_SANDBOX
-    // Fail
-    // @test
-    void case2()
-    {
-       WARN("gonna exit");
-  //     ::exit(1);
-    }
-#endif
-
-    // @test
-    void case3()
-    {
-    }
-
-    // @test
-    TEST(case4())
-    {
-      EXPECT_NE(2, 2);
-      INFO("still be here even if it's been failed");
-    }
-
-    // @test
-    void case5()
-    {
-      ASSERT_EQ(std::string("abc"), "abc");
-    }
-
-    // @test
-    void case6()
-    {
-      FAIL("not implemented yet.");
-    }
-
-    // @test
-    void case7()
-    {
-      unsigned int a = 0xabcdef01;
-      unsigned int b = 0xabcdef02;
-      ASSERT_SAME_DATA(&a, &b, sizeof(unsigned int));
-    }
-
-    // @test
-    void case8()
-    {
-      unsigned int a = 0xabcdef01;
-      unsigned int b = 0xabcdef01;
-      ASSERT_SAME_DATA(&a, &b, sizeof(unsigned int));
-    }
-
-    // @test
-    void case9()
-    {
-       ASSERT_DELTA(10, 9.9, 0.01);
-    }
-
-    // @test
-    void case10()
-    {
-       ASSERT_DELTA(10, 9.9, 0.1);
-    }
-
-    void testCase11()
-    {
-      ASSERT_THROWS_NOTHING(case3());
-    }
-
-#if 0
-    void testCase12()
-    {
-      TS_ASSERT_THROWS(testCase3(), std::exception);
-    }
-#endif
-
-    // @test
-    void case13()
-    {
-      ASSERT_THROWS(case3(), std::exception);
-    }
-
-    // @test
-    void case14()
-    {
-      ASSERT_THROWS(case3(), std::exception);
-    }
-};
-
-
-FIXTURE(ChineseFixture)
-{
-    TEST(æµ‹è¯•ç”¨ä¾‹1: "1+1=2")
-    {
-       ASSERT_NE(2, 1+1);
-    }
-
-    TEST(æµ‹è¯•ç”¨ä¾‹2=>identical strings should equals each other)
-    {
-      TS_ASSERT(std::string("abc") == std::string("abcd"));
-    }
-
-    TEST(what ever it is)
-    {
-      int a = 1;
-      int b = 2;
-
-      TS_ASSERT_EQUALS(a, b);
-    }
+	TEST(EXPECT_XXX)
+	{
+		EXPECT_TRUE(false);
+		EXPECT_FALSE(true);
+		EXPECT_EQ(1, 2);
+		EXPECT_NE(1, 1);
+	}
 };
