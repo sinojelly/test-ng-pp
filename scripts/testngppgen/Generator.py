74d47772634e803ad79a210239ee3f05bc7d274f
sinojelly <sinojelly@fea37274-fc4e-11de-be56-c15834921611>
2010-10-01 09:23:48 +0000

    modify python generator to support -d target_dir parameter, so can generate a cpp file for each test file. and generate cpp file if and only if the test file is newer.

--- scripts/testngppgen/Generator.py
+++ scripts/testngppgen/Generator.py
@@ -34,7 +34,7 @@ def getUsageString(longOpts):
 ##########################################################
 def getOpt(longOpts, argv):
    try:
-      return getopt.getopt(argv, 'i:e:o:', getAssignableOptStrings(longOpts))
+      return getopt.getopt(argv, 'i:e:o:d:', getAssignableOptStrings(longOpts))
    except getopt.GetoptError, err:
       print >> sys.stderr, str(err)
       usage()
@@ -42,12 +42,26 @@ def getOpt(longOpts, argv):
 
 ##########################################################
 def usage():
-   print "Usage:", "testngppgen", \
+   print "Usage:"
+   print "    testngppgen", \
       getUsageString(longOpts), \
       "[-e encoding] -o target testcase1 testcase2 ..."
+   print "    or"
+   print "    testngppgen", \
+      getUsageString(longOpts), \
+      "[-e encoding] -d target_dir testcase1 testcase2 ..."
 
 ##########################################################
 longOpts = []
+
+##########################################################
+def newerThan(file1, file2):
+    if os.path.exists(file1) :
+        file1_stat = os.stat(file1)
+        file2_stat = os.stat(file2)
+        if file1_stat.st_mtime >= file2_stat.st_mtime :
+             return True
+    return False
 
 ##########################################################
 def generate(argv):
@@ -57,13 +71,34 @@ def generate(argv):
 
    encoding = 'utf-8'
 
+   target_dir = None
+
    for o, a in optlist:
       if o == "-o":
          target = a
       elif o == "-e":
          encoding = a
+      elif o == "-d":
+	     target_dir = a
 
    inputEncoding = encoding
+
+   if target_dir != None:
+      if not os.path.exists(target_dir) :
+         os.makedirs(target_dir)
+      for fixture in fixtures:
+         absFixtures = []
+         h_file_path=fixture
+         if not os.path.isabs(fixture):
+            h_file_path = os.path.join(os.getcwd(), fixture)
+         absFixtures.append(h_file_path)
+         h_file = os.path.basename(fixture)
+         cpp_file = h_file.replace('.h', '.cpp')
+         cpp_file_path = os.path.join(target_dir, cpp_file)
+         if newerThan(cpp_file_path, h_file_path) :
+            continue
+         process(cpp_file_path, absFixtures, inputEncoding, encoding)
+      return
 
    if target == None:
       usage()
