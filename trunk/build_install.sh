5d669b470e080950c6e816dcdd8ae2fa7b63c0aa
sinojelly <sinojelly@fea37274-fc4e-11de-be56-c15834921611>
2010-10-02 07:28:24 +0000

    add build_install.sh

---
+++ trunk/build_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# build and install
+
+# You can change this testngpp install dir by yourself, it must be an absolute path.
+install_dir=/home/jelly/Programming/cpp-ut-project/cpp-ut-project/premake_version/tools/testngpp
+
+
+function build() {
+	mkdir $1 2>/dev/null
+	cd $1
+	cmake -DCMAKE_INSTALL_PREFIX=$install_dir $2
+	make install
+}
+
+build ../build_testngpp_to_install ../test-ng-pp
+
+cd ../test-ng-pp
+
+
